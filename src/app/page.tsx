4fc6d533c48ecbf2664df21b866f07232eac6777
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-07-30 21:08:45 +0530

    [integrated AWS for uploading of files]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FileUpload from "@/components/FileUpload";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
@@ -12,7 +13,7 @@ export default async function Home() {
   const isAuth = !!userId;
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 ">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
             <h1 className="text-5xl mr-3 font-semibold">Chat with any PDF!</h1>
@@ -29,10 +30,10 @@ export default async function Home() {
           </p>
           <div className="w-full mt-4">
             {isAuth ? (
-              <h1>File Upload </h1>
+              
+                <FileUpload />
             ) : (
               <Link href="/sign-in">
-                {" "}
                 <Button>
                   Login to get started!
                   <LogIn className="w-4 h-4 ml-2" />
